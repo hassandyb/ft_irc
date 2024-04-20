681de7079e29def7e0bb2d142376fd5ae86e2fb8
HASSAN ED-DYB <107430322+hassandyb@users.noreply.github.com>
2024-04-20 16:20:58 +0100

    Add files via upload

---
+++ ft_irc/CMD/cmd.cpp
@@ -0,0 +1,108 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cmd.cpp                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/27 13:46:11 by hed-dyb           #+#    #+#             */
+/*   Updated: 2024/04/20 16:20:10 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../HEADERS/server.hpp"
+
+
+std::vector<std::string>  ft_split_command(std::string Command)
+{
+
+
+    std::vector<std::string> Cmds;
+
+    // no : in the command
+    if(Command.find(":") == std::string::npos)
+    {
+        ft_split_with_spaces(Cmds, Command);
+        return Cmds;
+    }
+
+    // : exist in out coammnd
+    size_t pos = Command.find(":");
+    std::string after2points = Command.substr(pos + 1, Command.size() - pos - 1);
+    std::string befor2points = Command.substr(0, pos);
+
+    ft_split_with_spaces(Cmds, befor2points);
+    Cmds.push_back(after2points);
+    return Cmds;
+}
+
+bool server::ft_is_registred(client & Client, int Socket)
+{
+    if(Client.getRegestred() == false)
+    {
+        std::string msg = Client.getNickname() + " (451) :You have not registered";
+        ft_send(Socket, msg.c_str(), msg.size(), 0);
+        return false;
+    }
+    return true;
+}
+
+void server::ft_execute_command(std::string Command, client & Client, int Socket)
+{
+    std::vector<std::string> Cmds = ft_split_command(Command);
+
+    if(Cmds.size() == 0)
+        return; 
+    // Password stage and autotication satge 
+    
+    if(Cmds[0] == "PASS" || Cmds[0] == "pass")
+    {
+        ft_pass(Cmds, Client, Socket);
+        return ;
+    }
+    if(Cmds[0] == "USER" || Cmds[0] == "user")
+    {
+        ft_user(Cmds, Client, Socket);
+        return;
+    }
+    if(Cmds[0] == "NICK" || Cmds[0] == "nick")
+    {
+        ft_nick(Cmds, Client, Socket);
+        return;
+    }
+    if(ft_is_registred(Client, Socket) == false)
+        return;
+        
+    if(Cmds[0] == "JOIN" || Cmds[0] == "join")
+        ft_join(Cmds, Client, Socket);
+        
+    else if(Cmds[0] == "INVITE" || Cmds[0] == "invite")
+        ft_invite(Cmds, Client, Socket);
+
+    else if(Cmds[0] == "PRIVMSG" || Cmds[0] == "privmsg")
+        ft_privmsg(Cmds, Client, Socket);
+
+    else if(Cmds[0] == "MODE" || Cmds[0] == "mode")
+        ft_mode(Cmds, Client, Socket);
+
+
+
+
+    
+    // else if(Cmds[0] == "TOPIC" || Cmds[0] == "topic")
+    //     ft_topic(Cmds, Client, Socket);// enter clinet as reference so you can change it if you want same for all functions ..
+    // else if(Cmds[0] == "PART" || Cmds[0] == "part")
+    //     ft_part(Cmds, Client, Socket);
+    // else if(Cmds[0] == "KICK" || Cmds[0] == "kick")
+    //     ft_kick(Cmds, Client, Socket);
+    // else if(Cmds[0] == "QUIT" || Cmds[0] == "quit")
+    //     ft_quit(Cmds, Client, Socket);
+    else
+        std::cout << "else condition ..." << std::endl;
+
+
+}
+
+
+
+
---
+++ ft_irc/CMD/mode.cpp
@@ -0,0 +1,223 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mode.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hed-dyb <hed-dyb@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/20 16:19:45 by hed-dyb           #+#    #+#             */
+/*   Updated: 2024/04/20 16:19:47 by hed-dyb          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../HEADERS/server.hpp"
+
+bool ft_valid_limit(std::string max)
+{
+    if(max.find_first_not_of("+0123456789") != std::string::npos)
+        return false;
+    if(ft_string_to_size_t(max) >= 0)
+        return false;
+    return true;
+
+}         
+
+void server::ft_mode_invite(channel & Channel, client & Client, bool sign)
+{
+    // :a!a MODE #group -l == 
+    // bool server::ft_send(int socket, const void * buff, size_t len, int flags)
+    if(sign == true && Channel.getInvitaionStatus() == false)
+    {
+        Channel.setInvitaionStatus(true);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " +i\r\n";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+    }
+    if(sign == false && Channel.getInvitaionStatus() == true)
+    {
+        Channel.setInvitaionStatus(false);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " -i\r\n";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+    }
+}
+
+void server::ft_mode_topic(channel & Channel, client & Client, bool sign)
+{
+    if(sign == true && Channel.getTopicStatus() == false)
+    {
+        Channel.setTopicStatus(true);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " +t\r\n";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+    }
+    if(sign == false && Channel.getTopicStatus() == true)
+    {
+        Channel.setTopicStatus(false);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " -t\r\n";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+    }
+}
+
+void server::ft_mode_password(channel & Channel, client & Client, bool sign, std::string Password)
+{
+    if(Password.empty() == true)// case no passwrd entred
+    {
+        std::string msg = ": 696 " + Channel.getName() + " (k) * you must specifiy a parameter for the op mode\r\n";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+        return;
+    }
+    if(sign == true)// case +k
+    {
+        Channel.setPassword(Password);
+        Channel.setPassWordStatus(true);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " +k " + Password + "\r\n";
+        ft_send_msg_to_all(Channel.getMembers(), msg);
+        ft_send_msg_to_all(Channel.getAdmins(), msg); 
+    }
+    if(sign == false)// case -k
+    {
+
+        if(Channel.getPassword() != Password) // incorect password
+        {
+            std::string msg = ": 467 " + Channel.getName() + " Channel key already set.\r\n";
+            ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+        }
+        else //  :a!a MODE #group -k 
+        {
+
+            Channel.setPassWordStatus(false);
+            std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " -k\r\n";
+            ft_send_msg_to_all(Channel.getMembers(), msg);
+            ft_send_msg_to_all(Channel.getAdmins(), msg); 
+        }
+    }
+}
+
+void server::ft_mode_limit(channel & Channel, client & Client, bool sign, std::string max)
+{
+    if(sign == true && max.empty() == true)// error 1 : +l requires  limit arg 
+    {
+        std::string msg = ": 461 " + Channel.getName() + " MODE +l :Not enough parameters !\r\n";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+        return;
+    }
+    if(sign == true && ft_valid_limit(max) == false) // error 2 : +l require valid limit number
+    {
+        std::string msg = ":696 " + Channel.getName() + " +l Invalid mode parameter.";
+        ft_send(Client.getSocket(), msg.c_str(), msg.size(), 0);
+        return;
+    }
+
+    if(sign == true)// case +l
+    {
+        Channel.setLimit(ft_string_to_size_t(max));
+        Channel.setLimitStatus(true);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " +l " + max;
+        ft_send_msg_to_all(Channel.getMembers(), msg);
+        ft_send_msg_to_all(Channel.getAdmins(), msg); 
+        
+    }
+    if(sign == false)// case -l
+    {
+        Channel.setLimitStatus(false);
+        std::string msg = ":" + Client.getNickname() + "!" + Client.getUsername() + " MODE " + Channel.getName() + " -l";
+        ft_send_msg_to_all(Channel.getMembers(), msg);
+        ft_send_msg_to_all(Channel.getAdmins(), msg); 
+    }
+}
+
+void server::ft_mode(std::vector<std::string>  Cmds, client & Client, int Socket)
+{
+
+    if(Cmds.size() == 1)
+    {
+        std::string msg = " 461 " + Client.getNickname() + " :Not enough parameters !\r\n";
+        ft_send(Socket, msg.c_str(), msg.size(), 0);
+        return ;
+    }
+
+    if(this->ft_find_a_channel(Cmds[1]) == false)
+    {
+        std::string msg = ": 403 " + Client.getNickname() + " " + Cmds[1] + " :No such channel\r\n";
+        ft_send(Socket, msg.c_str(), msg.size(), 0);
+        return ;
+    }
+   channel Channel = ft_get_a_channel(Cmds[1]);
+    std::string Client_name = Client.getNickname();
+    if(Cmds.size() == 2 && (Channel.ft_find_client("Members", Client_name) == false || Channel.ft_find_client("Admins", Client_name) == false))
+    {
+        std::string msg1 = ": 324 " + Client.getNickname() + " " + Channel.getName() + " " + Channel.ft_get_mode() + "\r\n";
+        std::string msg2 = ": 329 " + Client.getNickname() + " " + Channel.getName() + " " + Channel.ft_get_creation_time() + "\r\n";
+        ft_send(Socket, msg1.c_str(), msg1.size(), 0);
+        ft_send(Socket, msg2.c_str(), msg2.size(), 0);
+        return ;
+    }
+    if(Channel.ft_find_client("Admins", Client_name) == false)
+    {
+         std::string msg = ": 482 " + Cmds[1] + " " + ":You're not a channel operator\r\n";
+         ft_send(Socket, msg.c_str(), msg.size(), 0);
+         return ;
+    }
+    std::string Modestr = Cmds[2];
+    size_t j = 3;// args ...
+    bool sign = true;
+    std::string empty;
+    
+    for(size_t i = 0; Modestr.size(); i++)
+    {
+        if(Modestr.at(i) == '+')
+            sign = true;
+        else if (Modestr.at(i) == '-')
+            sign = false;
+
+        else if (Modestr.at(i) == 'i')
+            ft_mode_invite(Channel, Client, sign);
+            
+        else if(Modestr.at(i) == 't')
+            ft_mode_topic(Channel, Client, sign);
+
+        else if(Modestr.at(i) == 'k')
+        {
+            if(j < Cmds.size())
+                ft_mode_password(Channel, Client, sign, Cmds[j]);
+            else
+                ft_mode_password(Channel, Client, sign, empty);
+                j++;
+        }
+        else if(Modestr.at(i) == 'l')
+        {
+            if(j < Cmds.size())
+                ft_mode_limit(Channel, Client, sign, Cmds[j]);
+            else
+                ft_mode_limit(Channel, Client, sign, empty);
+            j++;  
+        }
+        // else if(Modestr.at(i) == 'o')
+        // {
+        //     if(j < Cmds.size())
+        //         ft_mode_operator(Channel, Client, sign, Cmds[j]);
+        //     else
+        //         ft_mode_operator(Channel, Client, sign, empty);
+        //     j++;
+        // }
+        else
+        {
+            std::string msg = ": 472 " + Client.getNickname() + " " + Channel.getName() + " " + Modestr.at(i) + " :is unknown mode char to me\r\n";
+            ft_send(Socket, msg.c_str(), msg.size(), 0);
+        }
+
+            
+            
+    
+    }
+
+
+
+
+    
+
+
+    
+
+    
+
+    
+}
